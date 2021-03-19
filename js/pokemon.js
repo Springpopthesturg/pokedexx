73dd445e2f5292fae835651f2023d49ba77cf128
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-03-18 21:39:50 -0400

    h

---
+++ js/pokemon.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: 'Balbasaur' , type: ['poison' , 'grass'] , height: 7} ,
+  {name: 'Charmander' , type: 'fire' , height: 6} ,
+  {name: 'Squirtle' , type: 'water' , height: 5}
+]
