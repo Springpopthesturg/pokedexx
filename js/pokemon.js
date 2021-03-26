05d21ed5033cdcce3b3c52b230cc42307fad0d2c
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-03-26 19:25:37 -0400

    for task 1.3
    edited heights to what is on the pokedex site. added the for loop which has a conditional of a "height of 2.5.

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-  {name: 'Balbasaur' , type: ['poison' , 'grass'] , height: 7} ,
-  {name: 'Charmander' , type: 'fire' , height: 6} ,
-  {name: 'Squirtle' , type: 'water' , height: 5}
+  {name: 'Balbasaur' , type: ['poison' , 'grass'] , height: 2.04} ,
+  {name: 'Charmander' , type: 'fire' , height: 2} ,
+  {name: 'Charmeleon' , type: 'fire' , height: 3.07} ,
+  {name: 'Charizard' , type: 'fire' , height: 5.07} ,
+  {name: 'Squirtle' , type: 'water' , height: 1.04}
 ]
+
+
+for (let i= 0; i < pokemonList.length ; i++) //having i add against the list
+{
+  if(pokemonList[i].height > 2.5)
+  {
+    document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height})-This is a medium sized pokemon! <br>`) 
+  }
+  else document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height} <br>`)
+}
