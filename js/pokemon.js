ef3bc12f013e2f3a430f61ed564294b91e87be29
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-03-26 19:27:38 -0400

    conditional change
    changed conditional to fit requirements for assignment (only one pokemon meeting conditional criteria). Added notes to help when looking back at code.

--- js/pokemon.js
+++ js/pokemon.js
@@ -9,9 +9,9 @@ let pokemonList = [
 
 for (let i= 0; i < pokemonList.length ; i++) //having i add against the list
 {
-  if(pokemonList[i].height > 2.5)
+  if(pokemonList[i].height > 5) //this is the conditional
   {
-    document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height})-This is a medium sized pokemon! <br>`) 
+    document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height})-This is a LARGE pokemon! <br>`) //this dictates what happens when the pokemon is large enough for the conditional.
   }
-  else document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height} <br>`)
+  else document.write(`${pokemonList[i].name}  (height: ${pokemonList[i].height} <br>`) //this is for all who dont meet conditional.
 }
