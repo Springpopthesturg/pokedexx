ff7f63d294facacdffd9bc873bc448694bfb6622
Springpopthesturg <79934328+Springpopthesturg@users.noreply.github.com>
2021-04-08 00:36:08 -0400

    adding in the api Url
    getting rid of array

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,11 +1,6 @@
 let pokemonRepository = (function () {
-  let pokedex = [
-    {name: 'Balbasaur' , type: ['poison' , 'grass'] , height: 2.04} ,
-    {name: 'Charmander' , type: 'fire' , height: 2} ,
-    {name: 'Charmeleon' , type: 'fire' , height: 3.07} ,
-    {name: 'Charizard' , type: 'fire' , height: 5.07} ,
-    {name: 'Squirtle' , type: 'water' , height: 1.04}
-  ];
+  let pokedex = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   function getAll () {
     return pokedex;
